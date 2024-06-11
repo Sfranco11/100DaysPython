cb0b199509b0c4fa5ab6b66a6a9b2ffd9a6565c2
Sfranco11 <santiago.franco09@gmail.com>
2024-06-11 08:23:43 -0400

    Day 4

---
+++ day 4/Heads_Tails.py
@@ -0,0 +1,10 @@
+# Write your code below this line 👇
+# Hint: Remember to import the random module first. 🎲
+
+import random
+
+head_Tails = random.randint (0,1)
+if head_Tails == 1:
+    print("Heads")
+else:
+    print("Tails")
